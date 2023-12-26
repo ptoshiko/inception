7768a04bbf2f459c74eaa7f51d74bcb332639331
Your Angelina <angelina@gmail.com>
2023-12-26 20:42:54 +0000

    problem in wp-db connection

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # sleep 10
 
 # wp core download --allow-root --path='/var/www/wordpress'
@@ -52,7 +53,7 @@ else
 	#Inport env variables in the config file
 	sed -i "s/username_here/$SQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$SQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$SQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/localhost/192.168.112.1/g" wp-config-sample.php
 	sed -i "s/database_name_here/$SQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 
---
+++ srcs/requirements/wordpress/conf/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Starting WP"
+/usr/sbin/php-fpm7.4 -F
