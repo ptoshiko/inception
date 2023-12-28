39ac8a2b9f7010b3e51e13ce7ce2db60d0e8d5d3
Your Angelina <angelina@gmail.com>
2023-12-28 00:56:55 +0000

    WP works

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -1,38 +1,17 @@
-#!/bin/bash
+# #!/bin/bash
 
-#check if wp-config.php exist
-echo "$SQL_USER"
+# #check if wp-config.php exist
 
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
+# #Download wordpress and all config files
+# wget http://wordpress.org/latest.tar.gz
+# tar xfz latest.tar.gz
+# mv wordpress/* .
+# rm -rf latest.tar.gz
+# rm -rf wordpress
 
-	#Download wordpress and all config files
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
+# sed -e "s/username_here/$SQL_USER/g" wp-config-sample.php
+# sed -e "s/password_here/$SQL_PASSWORD/g" wp-config-sample.php
+# sed -e "s/localhost/$SQL_HOSTNAME/g" wp-config-sample.php
+# sed -e "s/database_name_here/$SQL_DATABASE/g" wp-config-sample.php
 
-	#Import env variables in the config file
-	# sed -i "" -e "s/username_here/$SQL_USER/g" wp-config-sample.php
-  	# sed -i "" -e "s/password_here/$SQL_PASSWORD/g" wp-config-sample.php
-  	# sed -i "" -e "s/localhost/$SQL_HOSTNAME/g" wp-config-sample.php
-  	# sed -i "" -e "s/database_name_here/$SQL_DATABASE/g" wp-config-sample.php
-
-	sed -e "s/username_here/$SQL_USER/g" -e "s/password_here/$SQL_PASSWORD/g" \
-	-e "s/localhost/$SQL_HOSTNAME/g" -e "s/database_name_here/$SQL_DATABASE/g" \
-	wp-config-sample.php > wp-config.php
-
-	# sed -i "s/username_here/$SQL_USER/g" wp-config-sample.php
-	# sed -i "s/password_here/$SQL_PASSWORD/g" wp-config-sample.php
-	# sed -i "s/localhost/$SQL_HOSTNAME/g" wp-config-sample.php
-	# # sed -i "s/localhost/192.168.112.1/g" wp-config-sample.php
-	# sed -i "s/database_name_here/$SQL_DATABASE/g" wp-config-sample.php
-	
-	cp wp-config-sample.php wp-config.php
-
-fi
-
-exec "$@"
+# mv wp-config-sample.php wp-config.php
--- srcs/requirements/wordpress/conf/start.sh
+++ srcs/requirements/wordpress/conf/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# mv /var/www/html/wp-config-sample.php /aboba.php
+
 echo "Starting WP"
 /usr/sbin/php-fpm7.4 -F
