7fb722c0db8d68f52528d401e85bcd0310a4c745
Paola Toshiko <ptoshiko@paul-f5Ar4s2.clusters.42paris.fr>
2024-04-02 11:37:42 +0200

    mariadb root password

--- Makefile
+++ Makefile
@@ -22,4 +22,6 @@ clean:
 # 	docker network rm $$(docker network ls -q);\
 
 
+# docker stop $(docker ps -qa); docker rm $(docker ps -qa); docker rmi -f $(docker images -qa); docker volume rm $(docker volume ls -q); docker network rm $(docker network ls -q)2>/dev/null
+
 .PHONY: all re down clean
--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -6,7 +6,9 @@ echo "i am here"
 
 service mariadb start
 
-mysql -u root <<EOF
+mysqladmin -u root password `${SQL_ROOT_PASSWORD}`
+
+mysql -u root -p`${SQL_ROOT_PASSWORD}`<<EOF
 CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';
