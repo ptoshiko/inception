f6bac31ecce0eec66390b17883d6f2b901cede99
Paola Toshiko <ptoshiko@paul-f5Ar4s2.clusters.42paris.fr>
2024-04-02 12:03:25 +0200

    changes mariadb

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -6,9 +6,10 @@ echo "i am here"
 
 service mariadb start
 
-mysqladmin -u root password `${SQL_ROOT_PASSWORD}`
+# mysqladmin -u root password `${SQL_ROOT_PASSWORD}`
 
-mysql -u root -p`${SQL_ROOT_PASSWORD}`<<EOF
+# mysql -u root -p`${SQL_ROOT_PASSWORD}`<<EOF
+mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';
